6fd78f0198fb92f522cd9300932111bdbed57096
Russell Villanueva <villanuevarussell@yahoo.com>
2019-07-10 22:42:09 -0700

    Added JewelsAndStones.py and edited README.md

---
+++ JewelsAndStones.py
@@ -0,0 +1,47 @@
+'''
+This program tells you how many jewels are in a string of stones.
+The first parameter are the characters that are jewels
+The second parameter is list of stones.
+The program compares each stone if it is a Jewel and returns the number of jewels in the string
+'''
+
+def JewelsAndStones(j,s):
+	'''
+	#Using a Hash Map
+	#Different types of stones
+	Jewels = {}
+	jewelcount = 0
+
+	#splits into a list
+	j = list(j)
+	#puts each jewel into a table
+	for jewel in j:
+		Jewels[jewel] = jewel
+
+	for stones in s:
+		if stones in Jewels:
+			jewelcount += 1
+
+	return jewelcount
+
+	'''
+	#Quicker way to do this
+	jewelcount = 0
+
+	for stones in s:
+		if stones in j:
+			jewelcount += 1
+
+
+	return jewelcount
+	
+	
+
+	
+
+
+
+
+
+if __name__ == "__main__":
+	print(JewelsAndStones("aA","aAAbbbb"))
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Random-Coding-Exercises
-A list of random coding exercises I completed for practice
+A list of random coding exercises I completed for practice. The details of each problem are on the top of each script.
+
+How to run:
+1.Open Terminal
+2.Navigate to Directory
+3.type "python ./NameOfProgram"
+
+List of Exercises:
+1.FizzBuzz.py
+2.GoatLatin.py
+3.LongestWord.py
+4.MaxDifference.py
+5.JewelsAndStones.py
